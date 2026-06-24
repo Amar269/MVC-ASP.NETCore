c538809020e9b1fe1a560baedb6494f6a56554cc
Amarnath Kolla <amarrama269@gmail.com>
2026-06-24 16:10:27 +0530

    Added Model and Views fiels for the Item Model

---
+++ MVC-Project/Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "Nothing" };
+            return View(item);
+        }
+    }
+} 
